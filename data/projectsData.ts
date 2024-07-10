817ab73cdbc4de0cc796107857c9e1db172c80f0
Paul Lewis <lewi0231@flinders.edu.au>
2024-07-11 07:51:55 +0930

    init

---
+++ data/projectsData.ts
@@ -0,0 +1,29 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'KitHub.store',
+    description: `An e-commerce site that focuses on leveraging free tutorial kits.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Task-based life coaching web app',
+    description: `Imagine never `,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Recursive Trails: A thru-hiking / travel blog.',
+    description: `A travel / thru-hiking blog`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
